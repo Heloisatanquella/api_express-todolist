88d4529c01c2bc7c78d79cac5be4b2a7427ea896
heloisatanquella <heloisacristovao02@gmail.com>
2024-11-21 10:52:54 -0300

    feat: route post

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json())
 const port = 3000;
 mongoose.connect(
-  "mongodb+srv://heloisatanquella:<db_password>@apiexpress.4qfhd.mongodb.net/?retryWrites=true&w=majority&appName=apiexpress"
+  "mongodb+srv://heloisatanquella:ymH6ZYhBoNv9K67E@apiexpress.4qfhd.mongodb.net/?retryWrites=true&w=majority&appName=apiexpress"
 );
 
 const Task = mongoose.model("Task", { 
@@ -16,6 +17,14 @@ app.get("/tasks", (req, res) => {
   res.send("Hello world");
 });
 
+app.post("/tasks", async (req, res) => {
+    const task = new Task({
+        title: req.body.title,
+        description: req.body.description
+    })
+    await task.save()
+    res.send(task)
+})
 
 app.listen(port, () => {
   console.log("App running");
